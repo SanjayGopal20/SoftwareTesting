83b4a3e8eb79443461596e54a6596296615e95ee
Rahul <naikahul661@gmail.com>
2023-08-08 11:41:03 +0530

    driver file added

---
+++ src/main/java/driver.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class driver {
+
+    public static WebDriver webDriver(WebDriver driver){
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(opt);
+        return driver;
+    }
+}
